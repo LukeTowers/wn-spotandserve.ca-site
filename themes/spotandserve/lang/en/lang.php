2f79d5757d95a4fcf28e87b9428760335a00745f
Luke Towers <git@luketowers.ca>
2023-03-24 23:26:24 -0600

    Replace GA with Fathom Analytics & added 404 & error pages

--- themes/spotandserve/lang/en/lang.php
+++ themes/spotandserve/lang/en/lang.php
@@ -2,7 +2,6 @@
 
 return [
     'options' => [
-        'googleanalytics_id' => 'Google Analytics ID',
         'color_primary' => 'Primary Color',
         'color_secondary' => 'secondary Color',
     ],
